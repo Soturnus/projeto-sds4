e6a5b8bbeb70900ecf1363c1abfc7105c0b7e8ac
Soturnus <rivaldo.valhalla@gmail.com>
2021-09-06 12:58:16 -0300

    componentes estaticos basicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1>Olá Mundo!</h1>
-    </div>
+    <>
+      <div className="container">
+        <NavBar />
+        <h1 className="text-primary">Olá Mundo!</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
